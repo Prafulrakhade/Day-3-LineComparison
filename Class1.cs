d30d4dd5e10a30d45483d47c9cd4896d31013422
Praful <prafulrakhade02@gmail.com>
2022-03-11 11:10:31 +0530

    [praful] added: Uc-1-Cartesian System Program File

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisons
+{
+    public class Class1
+    {
+        double x1, y1, x2, y2;
+        double distance;
+        public void Length()
+        {
+            Console.WriteLine("Enter the Point 1 ( x1 , y1 : )");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Point 2 ( x2 , y2 : )");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            y2 = Convert.ToDouble(Console.ReadLine());
+            distance = Math.Sqrt(((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+            Console.WriteLine("The Length Between Point 1 and Point 2 = " + distance);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparisons
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 check = new Class1();
+            check.Length();
+        }
+    }
+}
